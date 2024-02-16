71f33bca42fb4d18de33eb342eb0a924bfe1e553
Choi Hojo <ubermensch100326@gmail.com>
2024-02-16 15:16:13 +0900

    docs: 인터셉터 처리 수정

--- backend/src/main/java/com/ssafy/msg/config/WebConfig.java
+++ backend/src/main/java/com/ssafy/msg/config/WebConfig.java
@@ -33,9 +33,8 @@ public class WebConfig implements WebMvcConfigurer {
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(userInterceptor)
 				.addPathPatterns("/**")
-//				.excludePathPatterns("/swagger-ui/**", "/swagger-resources/**", "/v3/api-docs/**", "/error/**")
-//				.excludePathPatterns("/user/sign-in/**", "/user/sign-up/**", "/user/sign-out/**", "/user/refresh/**", "/user/password/reset/**", "/test/**")
-				.excludePathPatterns("/user/sign-in/**", "/user/sign-out/**", "/user/refresh/**", "/user/password/reset/**", "/test/**")
+				.excludePathPatterns("/swagger-ui/**", "/swagger-resources/**", "/v3/api-docs/**", "/error/**")
+				.excludePathPatterns("/user/sign-in/**", "/user/sign-up/**", "/user/sign-out/**", "/user/refresh/**", "/user/password/reset/**", "/test/**")
 				.excludePathPatterns("/article/guest")
 				.excludePathPatterns("/scheduler/night/**");
 	}
